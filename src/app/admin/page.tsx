12c58e69ecb43f0e90eae3e3f498f3b3771567ac
daniel-burbridge-developer <daniel.burbridge.developer@gmail.com>
2025-03-01 22:10:34 +0800

    adding shadcn form

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,7 @@
 
 import { UTUploadDropzone } from "@/components/ut-dropzone-uploader";
 import Image from "next/image";
-import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const AdminPage = () => {
   return (
