12a811685c3308d4c6da96a87377d6cf98187de7
daniel-burbridge-developer <daniel.burbridge.developer@gmail.com>
2025-03-03 13:47:29 +0800

    item product uploading

--- src/components/admin-dashboard.tsx
+++ src/components/admin-dashboard.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import { getImages } from '@/server/db/queries';
-import { UTUploadDropzone } from './ut-dropzone-uploader';
+import { useState, useEffect } from "react";
+import { getImages } from "@/server/db/queries";
+import { UTUploadDropzone } from "./ut-dropzone-uploader";
 
 const AdminDashboard = () => {
   const [images, setImages] = useState<
@@ -25,43 +25,27 @@ const AdminDashboard = () => {
   }, []);
 
   return (
-    <div className='bg-slate-600 min-h-screen grid grid-rows-[auto,1fr]'>
-      <header className='bg-blue-600 text-white p-4 shadow'>
-        <h1 className='text-2xl font-bold'>Admin Dashboard</h1>
+    <div className="bg-slate-600 min-h-screen grid grid-rows-[auto,1fr]">
+      <header className="bg-blue-600 text-white p-4 shadow">
+        <h1 className="text-2xl font-bold">Admin Dashboard</h1>
       </header>
-      <div className='bg-pink-400 min-w-full flex-1'>
+      <div className="bg-pink-400 min-w-full flex-1">
         <UTUploadDropzone uploadCompleted={fetchImages} />
       </div>
 
       <section>
-        <h2 className='text-2xl font-semibold mb-4'>Select Product Image</h2>
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4'>
-          {images?.map((photo, index) => (
-            <div
-              key={index}
-              className='border rounded overflow-hidden hover:shadow-lg transition-shadow duration-200'
-            >
-              {images
-                ?.filter((image) => image.status === 'not assigned')
-                .map((image) => (
-                  <div key={image.id}>
-                    <img src={image.url} width={200} height={200} />
-                    <p>{image.status}</p>
-                  </div>
-                ))}
-            </div>
-          ))}
+        <h2 className="text-2xl font-semibold mb-4">Select Product Image</h2>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 m-2">
+          {images
+            ?.filter((image) => image.status === "not assigned")
+            .map((image) => (
+              <div key={image.id}>
+                <img src={image.url} width={200} height={200} />
+                <p>{image.status}</p>
+              </div>
+            ))}
         </div>
       </section>
-
-      <div>
-        {images?.map((image) => (
-          <div key={image.id}>
-            <img src={image.url} width={200} height={200} />
-            <p>{image.status}</p>
-          </div>
-        ))}
-      </div>
     </div>
   );
 };
