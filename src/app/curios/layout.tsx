9a09e5e038c2851636928525dd3fcb3aec2cf496
daniel-burbridge-developer <daniel.burbridge.developer@gmail.com>
2025-03-08 17:37:53 +0800

    renamed /me to /curios

---
+++ src/app/curios/layout.tsx
@@ -0,0 +1,30 @@
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await currentUser();
+
+  if (!user) {
+    return (
+      <div>
+        <h1>No logged in user - display login stuff</h1>
+      </div>
+    );
+  }
+
+  if (
+    !(
+      Array.isArray(user.publicMetadata?.roles) &&
+      user.publicMetadata.roles.includes("user")
+    )
+  ) {
+    <div>
+      <h1>You're not a user - How did we even get here?</h1>
+    </div>;
+  }
+
+  return <div>{children}</div>;
+}
