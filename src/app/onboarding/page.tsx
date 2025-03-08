5f374dcbad7e0075cdd9803337aca541d9583833
daniel-burbridge-developer <daniel.burbridge.developer@gmail.com>
2025-03-08 21:43:33 +0800

    set up onboarding with clerk

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useSignUp } from "@clerk/nextjs";
+
+const OnboardingPage = () => {
+  return (
+    <div className="w-full h-full bg-yellow-200">
+      <div className="bg-slate-600">Hello Time to onboard</div>
+    </div>
+  );
+};
+
+export default OnboardingPage;
