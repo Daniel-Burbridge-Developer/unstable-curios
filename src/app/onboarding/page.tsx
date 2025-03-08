4034c06b280264742e2171bb9de7aeda2a7efc58
daniel-burbridge-developer <daniel.burbridge.developer@gmail.com>
2025-03-08 22:03:32 +0800

    fixed SSR issue with hydration

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,12 +1,51 @@
 "use client";
-import { useSignUp } from "@clerk/nextjs";
 
-const OnboardingPage = () => {
+import * as React from "react";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { completeOnboarding } from "./_actions";
+
+export default function OnboardingComponent() {
+  const [error, setError] = React.useState("");
+  const { user } = useUser();
+  const router = useRouter();
+
+  const handleSubmit = async (formData: FormData) => {
+    const res = await completeOnboarding(formData);
+    if (res?.message) {
+      // Reloads the user's data from the Clerk API
+      await user?.reload();
+      router.push("/");
+    }
+    if (res?.error) {
+      setError(res?.error);
+    }
+  };
+
   return (
-    <div className="w-full h-full bg-yellow-200">
-      <div className="bg-slate-600">Hello Time to onboard</div>
+    <div>
+      <h1>Welcome</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          handleSubmit(formData);
+        }}
+      >
+        <div>
+          <label>Application Name</label>
+          <p>Enter the name of your application.</p>
+          <input type="text" name="applicationName" required />
+        </div>
+
+        <div>
+          <label>Application Type</label>
+          <p>Describe the type of your application.</p>
+          <input type="text" name="applicationType" required />
+        </div>
+        {error && <p className="text-red-600">Error: {error}</p>}
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-};
-
-export default OnboardingPage;
+}
