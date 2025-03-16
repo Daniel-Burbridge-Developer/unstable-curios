d5d3bae844d3d618a81488f19f4c54b9ba0b9365
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2025-03-16 17:23:32 +0800

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
-import { AppStoreProvider } from "@/app/providers/app-store-provider";
-import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Providers } from "@/app/providers/tanstack-query-provider";
-import { Toaster } from "@/components/ui/sonner";
-import TopNav from "@/components/topnav";
+import { AppStoreProvider } from '@/app/providers/app-store-provider';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Providers } from '@/app/providers/tanstack-query-provider';
+import { Toaster } from '@/components/ui/sonner';
+import { Analytics } from '@vercel/analytics/react';
+
+import TopNav from '@/components/topnav';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Unstable-Curios",
-  description: "A place for all your collections",
+  title: 'Unstable-Curios',
+  description: 'A place for all your collections',
 };
 
 export default function RootLayout({
@@ -31,23 +33,24 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
+      <html lang='en' suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
+            attribute='class'
+            defaultTheme='dark'
             enableSystem
             disableTransitionOnChange
           >
             <Providers>
               <AppStoreProvider>
-                <div className="grid grid-rows-[auto_1fr] min-h-screen">
+                <div className='grid grid-rows-[auto_1fr] min-h-screen'>
                   <TopNav />
-                  <div className="bg-red-300">
+                  <div className='bg-red-300'>
                     {children}
                     <Toaster richColors />
+                    <Analytics />
                   </div>
                 </div>
               </AppStoreProvider>
