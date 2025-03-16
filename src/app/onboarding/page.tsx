5656a98074564c5fe4b2f05e7736d7ac7b03fab9
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2025-03-16 17:47:16 +0800

    headache with the prod version, database runs slow?

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { useUser } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { completeOnboarding } from "./_actions";
+import * as React from 'react';
+import { useUser } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
+import { completeOnboarding } from './_actions';
 
 export default function OnboardingComponent() {
-  const [error, setError] = React.useState("");
+  const [error, setError] = React.useState('');
   const { user, isLoaded } = useUser();
   const router = useRouter();
 
@@ -18,7 +18,8 @@ export default function OnboardingComponent() {
     if (res?.message) {
       // Reloads the user's data from the Clerk API
       await user?.reload();
-      router.push("/");
+      //should move passed the onboarding
+      router.push('/');
     }
     if (res?.error) {
       setError(res?.error);
@@ -38,16 +39,16 @@ export default function OnboardingComponent() {
         <div>
           <label>Application Name</label>
           <p>Enter the name of your application.</p>
-          <input type="text" name="applicationName" required />
+          <input type='text' name='applicationName' required />
         </div>
 
         <div>
           <label>Application Type</label>
           <p>Describe the type of your application.</p>
-          <input type="text" name="applicationType" required />
+          <input type='text' name='applicationType' required />
         </div>
-        {error && <p className="text-red-600">Error: {error}</p>}
-        <button type="submit">Submit</button>
+        {error && <p className='text-red-600'>Error: {error}</p>}
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
