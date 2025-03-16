2dab4dc5e41ea9ba9a0e449e19f1ae668e062e10
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2025-03-16 14:59:03 +0800

    populated fake data - deploying

---
+++ src/app/api/organisations/[organisationId]/collections/[collectionId]/items/[itemId]/route.ts
@@ -0,0 +1,13 @@
+// FAKE DATA SO IT BUILDS
+import { getOrganisations, createOrganisation } from '@/server/db/queries';
+
+export async function GET() {
+  const organisations = await getOrganisations();
+  return new Response(JSON.stringify(organisations), { status: 200 });
+}
+
+export async function POST(request: Request) {
+  const data = await request.json();
+  const newOrganisation = await createOrganisation(data);
+  return new Response(JSON.stringify(newOrganisation), { status: 201 });
+}
